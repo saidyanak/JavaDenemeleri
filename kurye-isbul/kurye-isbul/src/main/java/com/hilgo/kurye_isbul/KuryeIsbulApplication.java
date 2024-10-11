0f4359f776360306c4c63779d03d5cfa77b93d62
said yanak <75083985+saidyanak@users.noreply.github.com>
2024-10-11 20:07:47 +0300

    Yeni dosya

---
+++ kurye-isbul/kurye-isbul/src/main/java/com/hilgo/kurye_isbul/KuryeIsbulApplication.java
@@ -0,0 +1,13 @@
+package com.hilgo.kurye_isbul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuryeIsbulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuryeIsbulApplication.class, args);
+	}
+
+}
---
+++ kurye-isbul/kurye-isbul/src/test/java/com/hilgo/kurye_isbul/KuryeIsbulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hilgo.kurye_isbul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuryeIsbulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
